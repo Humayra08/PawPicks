e0b152ca2a8d637d6b25e540edfaa0905531f8ae
Humayra Binte Kazal <humayrabintekazal@gmail.com>
2025-08-16 02:51:28 +0600

    Built Home Page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -15,3 +15,19 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './Components/HomePage.jsx'; // ✅ directly importing your component
+import reportWebVitals from './reportWebVitals.js';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
+

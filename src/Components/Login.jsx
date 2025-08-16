34bd81acb9c893bf314642b3e7defcfbed1d2b55
Zamila18 <jamilamuhammad18052000@gmail.com>
2025-08-17 00:54:05 +0600

    login and reg is created

---
+++ src/Components/Login.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default function Login() {
+  // Blank white page for now
+  return <div style={{ minHeight: '100vh', background: '#ffffff' }} />;
+}
---
+++ src/Components/Registration.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default function Registration() {
+  // Blank white page for now
+  return <div style={{ minHeight: '100vh', background: '#ffffff' }} />;
+}
